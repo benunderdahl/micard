2cfb28a4cf0f9f9d432293367762fdbf2cbbd324
Code Classroom <benjamin.underdahl@proton.me>
2025-10-07 21:41:10 -0500

    added network image to circle avatar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp();
+  runApp(MyWidget());
 }
 
+
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Scaffold(
+    backgroundColor: Colors.blue[100],
+    body: SafeArea(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          CircleAvatar(
+            radius: 50.0,
+            backgroundImage: NetworkImage('https://avatarfiles.alphacoders.com/375/thumb-1920-375334.jpeg'),
+          ),
+        ]
+      ),
+    ),
+      ) 
+    );
+  }
+}
