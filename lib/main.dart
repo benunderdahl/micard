2ad6ca0f9f487feb6eade398c10c670f8c97165b
Code Classroom <benjamin.underdahl@proton.me>
2025-10-07 23:19:37 -0500

    centered content

--- lib/main.dart
+++ lib/main.dart
@@ -13,78 +13,73 @@ Widget build(BuildContext context) {
     home: Scaffold(
       backgroundColor: Colors.blue[100],
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            CircleAvatar(
-              radius: 100.0,
-              backgroundImage: NetworkImage(
-                'https://avatarfiles.alphacoders.com/375/thumb-1920-375334.jpeg',
-              ),
-            ),
-            Text(
-              'Power',
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Allison',
-              ),
-            ),
-            Text(
-              'Flutter Developer',
-              style: TextStyle(
-                fontSize: 30,
-                fontFamily: 'BBH_Sans_Bogle',
-                letterSpacing: 0.75,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            Card(
-              color: Colors.white,
-              margin: EdgeInsets.all(12),
-              child: Padding(
-                padding: EdgeInsets.all(8),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Image.asset(
-                      'icons/chainsaw.png',
-                      width: 45,
-                      height: 45,
-                    ),
-                    Text(
-                      "+44 123 456 7890",
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
-                        fontFamily: 'BBH_Sans'
-                      ),
-                    ),
-                  ],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 100.0,
+                backgroundImage: NetworkImage(
+                  'https://avatarfiles.alphacoders.com/375/thumb-1920-375334.jpeg',
                 ),
               ),
-            ),
-            Card(
-              color: Colors.white,
-              margin: EdgeInsets.all(12),
-              child: Padding(
-                padding: EdgeInsets.all(8),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Image.asset('icons/devil.png', width: 45, height: 45
-                    ),
-                    Text("power@bloodfiend.com", style: TextStyle(
-                      fontFamily: 'BBH_Sans', fontWeight: FontWeight.w500, fontSize: 16
-                      ),
-                      ),
-                  ],
+              Text(
+                'Power',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Allison',
                 ),
               ),
-            ),
-          ],
+              Text(
+                'Flutter Developer',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontFamily: 'BBH_Sans_Bogle',
+                  letterSpacing: 0.75,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.all(12),
+                child: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: ListTile(
+                    leading: Image.asset('icons/chainsaw.png'),
+                    title: Text('+44 123 456 7890', style: TextStyle(
+                      fontFamily: 'BBH Sans',
+                      fontSize: 20,
+                      letterSpacing: 2,
+                    ),
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.all(12),
+                child: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: ListTile(
+                    leading: Image.asset('icons/devil.png'),
+                    title: Text('power@bloodfiend.com',
+                    style: TextStyle(
+                      fontFamily: 'BBH Sans',
+                      fontSize: 20,
+                      letterSpacing: 1,
+                    ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     ),
   );
-}}
+ }
+}
+
+
